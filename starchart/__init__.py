42dfcb8119952a614f6637c15aeb7779ad303b8e
strongbugman <strongbugman@gmail.com>
2019-02-20 17:54:23 +0800

    Release 0.3.1

--- starchart/__init__.py
+++ starchart/__init__.py
@@ -3,4 +3,4 @@ from .endpoints import Schema, SwaggerUI, RedocUI
 from .extensions import Starchart
 
 __all__ = ["SchemaGenerator", "SwaggerUI", "RedocUI", "Schema", "Starchart"]
-__version__ = "0.3.0"
+__version__ = "0.3.1"
